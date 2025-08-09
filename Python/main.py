971bec3948ee387dc0e98cc3622dfd28e0ae40ec
unknown <r_faizaan@yahoo.ca>
2025-08-09 15:44:26 -0400

    Add apify integration

--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,9 @@
 from parser import find_deals
-from scraper import load_data
+from scraper import load_data_from_apify
 from constants import KEYWORDS
-import os
-import json
 
 def main():
-    # Get the instagram post data in JSON format, obtained through Apify
-    file_path = os.path.join("..", "Datasets", "InstagramPosts.json")
-
-    data = load_data(file_path)
+    data = load_data_from_apify()
     deals = find_deals(data, KEYWORDS)
     for deal in deals:
         print(f"{deal.match_count} matches in {deal.url} with {deal.matches}")
--- Python/scraper.py
+++ Python/scraper.py
@@ -1,11 +1,14 @@
-import json
-import os
+from apify_client import ApifyClient
+from config import APIFY_KEY
 
-# Obtaining data in JSON format
-def load_data(filepath):
-    try:
-        with open(filepath, encoding="utf8") as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        print(f"Error loading JSON data: {e}")
-        exit(1)
+def load_data_from_apify():
+    # Initialize ApifyClient
+    apify_client = ApifyClient(APIFY_KEY)
+
+    # Get dataset
+    dataset = apify_client.dataset('PLVAebVRSH2leQzWe')
+
+    # Get data from dataset
+    dataset_items = dataset.list_items()
+
+    return dataset_items.items
